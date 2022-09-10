993e03527cbd6ce7d94f1729374b374a6672bf32
News <samrat.subratadas@gmail.com>
2022-09-10 23:11:52 +0530

    fixd head

--- pages/componets/Navbar.js
+++ pages/componets/Navbar.js
@@ -1,171 +1,201 @@
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import $  from 'jquery';
+
 import { BsArrowRightShort,BsChevronRight, BsFillGridFill, BsFillShieldLockFill,BsFillBookmarkFill,BsLayersFill, BsCloudHaze2Fill, BsFileEarmarkBarGraphFill,BsKeyFill } from "react-icons/bs";
 
 import { FaAngleRight, FaBars } from "react-icons/fa";
 
 
-
 function Navbar(){
+
+    const [scroll, setScroll] = useState(false);
+ useEffect(() => {
+   window.addEventListener("scroll", () => {
+     setScroll(window.scrollY > 50);
+   });
+ }, []);
 
     const router = useRouter()
 
 
     return(
         <>
-        <nav className="navbar navbar-expand-lg navbar-light float-start w-100">
-         <div className="container">
-            <Link href='/' className="navbar-brand">
-             <a> 
-                <Image
-                src="/logo.png"
-                alt="Picture of the author"
-                width={177}
-                height={33}
-                />
-            </a>
-            </Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasmenu" >
-                 <FaBars/>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li className={router.pathname == "/" ? "active" : ""}>
-                        <Link href='/' > 
-                            <span className="nav-link">  Home </span>
-                        </Link>
-                    </li>
-                    <li className={router.pathname == "/about" ? "active" : ""}>
-                        <Link href='/about' > 
-                            <span className="nav-link">  About</span>
-                        </Link>
-                    </li>
-                    <li className="dropdown dropdown-mega position-static">
-                        <a className="nav-link mega-menu-a dropdown-toggle " href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Services
-                        </a>
-                        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                           <div className='mega-content'>
-                              
-                                <div className='row row-cols-1 row-cols-lg-4'>
-                                    <div className='col'>
-                                        
-                                            <h5> 
-                                                <Link href='/services/aicpasocreports'> 
+        <div className={scroll ? "bg-black" : "bg-white"}>
+            <nav className="navbar navbar-expand-lg navbar-light float-start w-100">
+            <div className="container">
+                <Link href='/' className="navbar-brand">
+                <div className='logo-div'>
+                   <div className='fiex-logo'>
+                        <Image
+                        src="/logo.png"
+                        alt="Picture of the author"
+                        width={177}
+                        height={33}
+                        
+                        />
+                   
+                   </div> 
+                   <div className='color-logo'>
+                        <Image
+                        src="/logo-color.png"
+                        alt="Picture of the author"
+                        
+                        width={177}
+                        height={33}
+                       />
+                   
+                   </div>
+                    
+
+                    
+                </div>
+               
+                </Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasmenu" >
+                    <FaBars/>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className={router.pathname == "/" ? "active" : ""}>
+                            <Link href='/' > 
+                                <span className="nav-link">  Home </span>
+                            </Link>
+                        </li>
+                        <li className={router.pathname == "/about" ? "active" : ""}>
+                            <Link href='/about' > 
+                                <span className="nav-link">  About</span>
+                            </Link>
+                        </li>
+                        <li className="dropdown dropdown-mega position-static">
+                            <a className="nav-link mega-menu-a dropdown-toggle " href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Services
+                            </a>
+                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <div className='mega-content'>
+                                
+                                    <div className='row row-cols-1 row-cols-lg-4'>
+                                        <div className='col'>
+                                            
+                                                <h5> 
+                                                    <Link href='/services/aicpasocreports'> 
+                                                        <a className='comon-link0'>
+                                                        <figure className='sm-icbn m-0'>
+                                                        <Image src="/pri.png"
+                                                        layout="fill"/>
+                                                        </figure> AICPA SOC 1, 2, 3 Reports <BsChevronRight/>
+                                                        </a>
+                                                    </Link>
+                                                </h5>
+                                        </div>
+
+                                        <div className='col'>
+                                            <h5>
+                                                <Link href='/services/cybersecurity'> 
                                                     <a className='comon-link0'>
-                                                     <figure className='sm-icbn m-0'>
-                                                       <Image src="/pri.png"
-                                                       layout="fill"/>
-                                                     </figure> AICPA SOC 1, 2, 3 Reports <BsChevronRight/>
+                                                        <figure className='sm-icbn m-0'>
+                                                        <Image src="/sei.png"
+                                                        layout="fill"/>
+                                                        </figure>
+                                                        Cybersecurity 
+                                                        <BsChevronRight/>
                                                     </a>
                                                 </Link>
                                             </h5>
-                                    </div>
+                                        </div>
 
-                                    <div className='col'>
-                                        <h5>
-                                            <Link href='/services/cybersecurity'> 
-                                                <a className='comon-link0'>
+                                        <div className='col'>
+                                            <h5>
+                                                <Link href='/services/cloudsecurity'> 
+                                                    <a className='comon-link0'>
+                                                        <figure className='sm-icbn m-0'>
+                                                        <Image src="/cld.png"
+                                                        layout="fill"/>
+                                                        </figure>
+                                                        Cloud Security 
+                                                        <BsChevronRight/>
+                                                    </a>
+                                                </Link>
+                                            </h5>
+                                        </div>
+
+                                        <div className='col'>
+                                            <h5> 
+                                                <Link href='/services/privacy'> 
+                                                    <a className='comon-link0'>
+                                                        <figure className='sm-icbn m-0'>
+                                                        <Image src="/pri.png"
+                                                        layout="fill"/>
+                                                        </figure>
+                                                        Privacy 
+                                                        <BsChevronRight/>
+                                                    </a>
+                                                </Link>
+                                            </h5>
+                                        </div>
+
+                                        <div className='col'>
+                                            <h5> 
+                                                <Link href='/services/blockchain'> 
+                                                    <a className='comon-link0'>
                                                     <figure className='sm-icbn m-0'>
-                                                       <Image src="/sei.png"
-                                                       layout="fill"/>
-                                                     </figure>
-                                                    Cybersecurity 
-                                                    <BsChevronRight/>
-                                                </a>
-                                            </Link>
-                                        </h5>
-                                    </div>
+                                                        <Image src="/block.png"
+                                                        layout="fill"/>
+                                                        </figure>
+                                                        Blockchain Assessments
+                                                        <BsChevronRight/>
+                                                    </a>
+                                                </Link>
+                                            </h5>
+                                        </div>
 
-                                    <div className='col'>
-                                        <h5>
-                                            <Link href='/services/cloudsecurity'> 
-                                                <a className='comon-link0'>
+                                        <div className='col'>
+                                            <h5> 
+                                                <Link href='/services/iso-certification-services'> 
+                                                    <a className='comon-link0'>
                                                     <figure className='sm-icbn m-0'>
-                                                       <Image src="/cld.png"
-                                                       layout="fill"/>
-                                                     </figure>
-                                                     Cloud Security 
-                                                    <BsChevronRight/>
-                                                </a>
-                                            </Link>
-                                        </h5>
+                                                        <Image src="/iso.png"
+                                                        layout="fill"/>
+                                                        </figure>
+                                                        ISO Certification Services 
+                                                        <BsChevronRight/>
+                                                    </a>
+                                                </Link>
+                                            </h5>
+                                        </div>
+
+                                        
+
+                                        
                                     </div>
+                            </div>
+                            </ul>
+                        </li>
+                        <li className={router.pathname == "/contact" ? "active" : ""}>
+                            <Link href='/contact' > 
+                                <span className="nav-link">  Contact </span>
+                            </Link>
+                        </li>
 
-                                    <div className='col'>
-                                        <h5> 
-                                            <Link href='/services/privacy'> 
-                                                <a className='comon-link0'>
-                                                    <figure className='sm-icbn m-0'>
-                                                       <Image src="/pri.png"
-                                                       layout="fill"/>
-                                                     </figure>
-                                                     Privacy 
-                                                     <BsChevronRight/>
-                                                </a>
-                                            </Link>
-                                        </h5>
-                                    </div>
-
-                                    <div className='col'>
-                                        <h5> 
-                                            <Link href='/services/blockchain'> 
-                                                <a className='comon-link0'>
-                                                   <figure className='sm-icbn m-0'>
-                                                       <Image src="/block.png"
-                                                       layout="fill"/>
-                                                     </figure>
-                                                    Blockchain Assessments
-                                                    <BsChevronRight/>
-                                                </a>
-                                            </Link>
-                                        </h5>
-                                    </div>
-
-                                    <div className='col'>
-                                        <h5> 
-                                            <Link href='/services/iso-certification-services'> 
-                                                <a className='comon-link0'>
-                                                   <figure className='sm-icbn m-0'>
-                                                       <Image src="/iso.png"
-                                                       layout="fill"/>
-                                                     </figure>
-                                                     ISO Certification Services 
-                                                    <BsChevronRight/>
-                                                </a>
-                                            </Link>
-                                        </h5>
-                                    </div>
-
-                                    
-
-                                    
-                                </div>
-                           </div>
-                        </ul>
-                    </li>
-                    <li className={router.pathname == "/contact" ? "active" : ""}>
-                        <Link href='/contact' > 
-                            <span className="nav-link">  Contact </span>
-                        </Link>
-                    </li>
-
-                    <li>
-                        <Link href='/about' > 
-                            <span className="btn lets-btn1"> Send a message </span>
-                        </Link>
-                    </li>
-                
-                
+                        <li>
+                            <Link href='/about' > 
+                                <span className="btn lets-btn1"> Send a message </span>
+                            </Link>
+                        </li>
                     
+                    
+                        
+                    
+                    </ul>
                 
-                </ul>
-            
+                </div>
+                
             </div>
-            
-         </div>
-        </nav>
+            </nav>
+        </div>
+
 
 
         
